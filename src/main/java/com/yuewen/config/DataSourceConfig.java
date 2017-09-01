df4390f37af9e520e4468e859e19fdd278b95e38
p_yxiaoduan <p_yxiaoduan@tencent.com>
2017-09-01 21:19:34 +0800

    initial

--- src/main/java/com/yuewen/config/DataSourceConfig.java
+++ src/main/java/com/yuewen/config/DataSourceConfig.java
@@ -2,16 +2,14 @@ package com.yuewen.config;
 
 import com.github.pagehelper.PageInterceptor;
 import com.yuewen.constants.ConfigFileCons;
-import com.yuewen.util.DynamicDBUtil;
+import com.yuewen.util.DynamicDBUtils;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import java.util.Properties;
@@ -36,8 +34,8 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public DynamicDBUtil dynamicDataSource() {
-        DynamicDBUtil dynamicDataSource = new DynamicDBUtil(dataSource(), config.dbname, config.num);
+    public DynamicDBUtils dynamicDataSource() {
+        DynamicDBUtils dynamicDataSource = new DynamicDBUtils(dataSource(), config.dbname, config.num);
         return dynamicDataSource;
 
     }
@@ -63,7 +61,7 @@ public class DataSourceConfig {
     }
 
     @Bean
-    public DataSourceTransactionManager transactionManager(DynamicDBUtil dynamicDataSource) {
+    public DataSourceTransactionManager transactionManager(DynamicDBUtils dynamicDataSource) {
         return new DataSourceTransactionManager(dynamicDataSource);
     }
 }
