45a654d26123c9dc45368c78996e74e50a8d4916
p_yxiaoduan <p_yxiaoduan@tencent.com>
2017-09-01 21:09:01 +0800

    initial

--- src/main/java/com/yuewen/constants/ConfigFileCons.java
+++ src/main/java/com/yuewen/constants/ConfigFileCons.java
@@ -1,7 +1,6 @@
 package com.yuewen.constants;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
@@ -12,23 +11,6 @@ import org.springframework.stereotype.Component;
 @Component
 @PropertySource("data.properties")
 public class ConfigFileCons {
-    //redis
-    @Value("${redis.host}")
-    public String host;
-    @Value(("${redis.port}"))
-    public int port;
-    @Value("${redis.password}")
-    public String redis_password;
-    @Value("${redis.timeout}")
-    public int timeout;
-    @Value("${redis.db}")
-    public int db;
-    @Value("${redis.poolSize}")
-    public int poolSize;
-    @Value("${redis.maxIdle}")
-    public int maxIdle;
-    @Value("${redis.maxWait}")
-    public int maxWait;
 
     @Value("${currentContext}")
     public String currentContext;
--- src/test/java/SpringTest.java
+++ src/test/java/SpringTest.java
@@ -1,6 +1,5 @@
 import com.yuewen.config.AppConfig;
 import com.yuewen.constants.ConfigFileCons;
-import com.yuewen.service.RedisClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,23 +22,9 @@ public class SpringTest {
 
 
 
-    @Autowired
-    private RedisClient redisClient;
-
     @Test
-    public void testMybatis() {
-//        List<Usertag> list = usertagMapper.getAllWithMapper();
-//        System.out.println(list);
-
-        String url = "%s";
-        System.out.println(String.format(url,"here"));
-
-    }
-
-
-    @Test
-    public void testRedis(){
-        redisClient.hgetAll("redis").ifPresent(System.out::println);
+    public void test(){
+        System.out.println("t");
     }
 
 
