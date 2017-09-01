d5162b239cba42ddaccab4a61835a616de76bfa9
p_yxiaoduan <p_yxiaoduan@tencent.com>
2017-09-01 21:38:18 +0800

    mapper+单数据源

---
+++ src/main/java/com/yuewen/util/LogUtis.java
@@ -0,0 +1,35 @@
+package com.yuewen.util;
+
+import com.mysql.jdbc.log.LogUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by duanyixiao on 2017/9/1.
+ */
+public class LogUtis {
+    private static final Logger log = LoggerFactory.getLogger(LogUtils.class);
+
+    public static void debug(String s){
+        log.debug(s);
+    }
+    public static void debug(String s,Object ... objects){
+        log.debug(s,objects);
+    }
+
+    public static void info(String s){
+        log.info(s);
+    }
+
+    public static void info(String s,Object... objects){
+        log.info(s,objects);
+    }
+
+    public static void error(String s){
+        log.info(s);
+    }
+
+    public static void error(String s,Object...objects){
+        log.error(s,objects);
+    }
+}
