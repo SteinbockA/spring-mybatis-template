670ecc18324ef9810d3e658d23c7e0bba87c43b0
p_yxiaoduan <p_yxiaoduan@tencent.com>
2017-09-05 20:38:53 +0800

    通用mapper+动态数据源

--- src/main/java/com/yuewen/config/AppConfig.java
+++ src/main/java/com/yuewen/config/AppConfig.java
@@ -1,16 +1,13 @@
 package com.yuewen.config;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * Created by duanyixiao on 2017/7/25.
  */
 @Configuration
 @ComponentScan(value = "com.yuewen")
-@MapperScan(value = "com.yuewen.mapper")
 public class AppConfig {
 
 }
