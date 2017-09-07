7c370c65765b985aa4d08a47d13f184429a2591b
p_yxiaoduan <p_yxiaoduan@tencent.com>
2017-09-07 20:40:15 +0800

    initial

--- src/main/java/com/yuewen/base/ResultGenerator.java
+++ src/main/java/com/yuewen/base/ResultGenerator.java
@@ -12,27 +12,34 @@ import static com.yuewen.constants.ReturnCons.*;
  */
 public class ResultGenerator {
 
-    public static void successResult(Object data,CommonOutParam outParam, Holder<Object> holder ) {
+    public static void successResult(Object data, Holder<com.yuewen.taf.servant.CommonOutParam> outParamHolder, Holder<Object> holder ) {
+        com.yuewen.taf.servant.CommonOutParam outParam = new com.yuewen.taf.servant.CommonOutParam();
         outParam.setCode(SUCCESS.code);
         outParam.setMessage(SUCCESS.toString());
+        outParamHolder.setValue(outParam);
         holder.setValue(data);
     }
 
-    public static void partitialSuccessResult(Object data, CommonOutParam outParam, Holder<Object> holder) {
+    public static void partitialSuccessResult(Object data, Holder<com.yuewen.taf.servant.CommonOutParam> outParamHolder, Holder<Object> holder) {
+        com.yuewen.taf.servant.CommonOutParam outParam = new com.yuewen.taf.servant.CommonOutParam();
         outParam.setCode(PARTIALSUCC.code);
         outParam.setMessage(PARTIALSUCC.toString());
         holder.setValue(data);
     }
 
-    public static void failResult(String message,Object data, CommonOutParam outParam, Holder<Object> holder) {
+    public static void failResult(String message, Object data, Holder<com.yuewen.taf.servant.CommonOutParam> outParamHolder, Holder<Object> holder) {
+        com.yuewen.taf.servant.CommonOutParam outParam = new com.yuewen.taf.servant.CommonOutParam();
         outParam.setCode(FAILED.code);
         outParam.setMessage(message);
+        outParamHolder.setValue(outParam);
         holder.setValue(data);
     }
 
-    public static void errorResult(ReturnCons returnCons,Object data, CommonOutParam outParam, Holder<Object> holder){
+    public static void errorResult(ReturnCons returnCons, Object data, Holder<com.yuewen.taf.servant.CommonOutParam> outParamHolder, Holder<Object> holder){
+        com.yuewen.taf.servant.CommonOutParam outParam = new com.yuewen.taf.servant.CommonOutParam();
         outParam.setCode(returnCons.code);
         outParam.setMessage(returnCons.toString());
+        outParamHolder.setValue(outParam);
         holder.setValue(data);
     }
 }
